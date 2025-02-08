238e04dc2b197eee0af8160fcbc2b02c47c1a324
JamesLTedder <JamesLTedder@smccme.edu>
2025-02-07 20:42:35 -0500

    added preamble

--- OneDrive/Documents/NetBeansProjects/GitHelloWorld/src/githelloworld/GitHelloWorld.java
+++ OneDrive/Documents/NetBeansProjects/GitHelloWorld/src/githelloworld/GitHelloWorld.java
@@ -14,6 +14,7 @@ public class GitHelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Here ye : important announcement");
         System.out.println("Hello World");
     }
     
